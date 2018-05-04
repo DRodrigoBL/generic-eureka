307fbd6c8cbcadaeff3268a713060383ddd59705
Rodrigo Bautista <rodrigo.bautista@ibm.com>
2018-05-04 11:13:34 -0500

    Se habilita el servidor de eureka

--- src/main/java/com/bancos/server/eureka/BancosEurekaApplication.java
+++ src/main/java/com/bancos/server/eureka/BancosEurekaApplication.java
@@ -2,8 +2,10 @@ package com.bancos.server.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BancosEurekaApplication {
 
 	public static void main(String[] args) {
