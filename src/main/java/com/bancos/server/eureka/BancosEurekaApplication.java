2fdeb0b9ec750ddea7f6754ff54051cc66012249
Rodrigo Bautista <rodrigo.bautista@ibm.com>
2018-05-04 10:49:51 -0500

    creacion del proyecto

---
+++ src/main/java/com/bancos/server/eureka/BancosEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.bancos.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosEurekaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bancos/server/eureka/bancoseureka/BancosEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bancos.server.eureka.bancoseureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BancosEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
